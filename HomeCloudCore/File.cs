285de9a6ba9d76805c9dd447b8fddc4b4094d725
Jason Chesters-salt <43147625+jasonTheHuman2101@users.noreply.github.com>
2023-11-26 21:19:21 +0000

    Created File class based on the fields of the database table it belongs to

---
+++ HomeCloudCore/File.cs
@@ -0,0 +1,11 @@
+﻿namespace HomeCloudCore
+{
+    public class File
+    {
+        public int FileID { get; set; }
+        public string FileName { get; set; }
+        public string MIMEType { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string PathToData { get; set; }
+    }
+}
