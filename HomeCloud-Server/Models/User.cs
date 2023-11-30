dbe1feec3b3a1b0ba18390a0dd2b196ea679830d
Jason Chesters-salt <43147625+jasonTheHuman2101@users.noreply.github.com>
2023-11-30 11:26:38 +0000

    Added class for user informaiton in table

---
+++ HomeCloud-Server/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using MySqlToolkit;
+
+namespace HomeCloud_Server.Models
+{
+    public class User
+    {
+        [MySqlColumn("UserID", true)]
+        public int UserID { get; set; }
+
+        [MySqlColumn("Username", false)]
+        public string Username { get; set;}
+
+        [MySqlColumn("Password", false)]
+        public string Password { get; set;}
+    }
+}
